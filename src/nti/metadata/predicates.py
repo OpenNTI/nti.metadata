544059c850cc6d4999a1a03cdba9ee4c19f089ee
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-26 20:36:58 +0000

    add predicate
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.metadata/trunk@54715 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/metadata/predicates.py
+++ src/nti/metadata/predicates.py
@@ -22,20 +22,6 @@ from nti.chatserver.interfaces import IUserTranscriptStorage
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import IPrincipalMetadataObjectsIntIds
 
-@component.adapter(IUser)
-@interface.implementer(IPrincipalMetadataObjectsIntIds)
-class _ContainedPrincipalObjectsIntIds(object):
-
-	__slots__ = ()
-
-	def __init__(self, user):
-		self.user = user
-
-	def iter_intids(self):
-		user = self.user
-		for uid in user.iter_intids(only_ntiid_containers=True):
-			yield uid
-
 def user_messageinfo_iter_intids(user, intids=None, broken=None):
 	intids = component.getUtility(zope.intid.IIntIds) if intids is None else intids
 	storage = IUserTranscriptStorage(user)
@@ -58,6 +44,20 @@ def user_messageinfo_iter_intids(user, intids=None, broken=None):
 
 @component.adapter(IUser)
 @interface.implementer(IPrincipalMetadataObjectsIntIds)
+class _ContainedPrincipalObjectsIntIds(object):
+
+	__slots__ = ()
+
+	def __init__(self, user):
+		self.user = user
+
+	def iter_intids(self):
+		user = self.user
+		for uid in user.iter_intids(only_ntiid_containers=True):
+			yield uid
+
+@component.adapter(IUser)
+@interface.implementer(IPrincipalMetadataObjectsIntIds)
 class _MessageInfoPrincipalObjectsIntIds(object):
 
 	__slots__ = ()
@@ -68,3 +68,28 @@ class _MessageInfoPrincipalObjectsIntIds(object):
 	def iter_intids(self):
 		for uid in user_messageinfo_iter_intids(self.user):
 			yield uid
+
+@component.adapter(IUser)
+@interface.implementer(IPrincipalMetadataObjectsIntIds)
+class _MeetingsPrincipalObjectsIntIds(object):
+
+	__slots__ = ()
+
+	def __init__(self, user):
+		self.user = user
+
+	def iter_intids(self):
+		storage = IUserTranscriptStorage(self.user)
+		intids = component.getUtility(zope.intid.IIntIds) 
+		for meeting in storage.meetings:
+			try:
+				if IBroken.providedBy(meeting):
+					logger.warn("ignoring broken object %s", type(meeting))
+				else:
+					uid = intids.queryId(meeting)
+					if uid is None:
+						logger.warn("ignoring unregistered object %s", meeting)
+					else:
+						yield uid
+			except (POSError):
+				logger.error("ignoring broken object %s", type(meeting))
