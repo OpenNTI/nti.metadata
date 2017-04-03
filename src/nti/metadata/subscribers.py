bd4d4eefa617bbb302f4adb324c3eb6465032936
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-03 22:54:36 +0000

    divide into two modules
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.metadata/trunk@110083 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/metadata/subscribers.py
+++ src/nti/metadata/subscribers.py
@@ -21,17 +21,12 @@ from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
 from nti.dataserver.interfaces import IEntity
 
-from nti.dataserver.metadata_index import IX_CREATOR
-from nti.dataserver.metadata_index import IX_TAGGEDTO
-from nti.dataserver.metadata_index import IX_SHAREDWITH
-from nti.dataserver.metadata_index import IX_REVSHAREDWITH
-from nti.dataserver.metadata_index import IX_REPLIES_TO_CREATOR
-
 from nti.metadata import is_indexable
 from nti.metadata import metadata_queue
 from nti.metadata import dataserver_metadata_catalog
 
-from nti.zope_catalog.interfaces import IKeywordIndex
+from nti.metadata.utils import delete_entity_metadata
+from nti.metadata.utils import clear_replies_to_creator
 
 
 def query_uid(obj):
@@ -111,62 +106,12 @@ def _object_modified(modeled, event):
     queue_modified(modeled)
 
 
-def delete_entity_data(username):
-    result = 0
-    logger.info("Removing metadata data for user %s", username)
-    catalog = dataserver_metadata_catalog()
-    if catalog is not None:
-        username = username.lower()
-        index = catalog[IX_CREATOR]
-        query = {IX_CREATOR: {'any_of': (username,)}}
-        results = catalog.searchResults(**query)
-        for uid in results.uids:
-            index.unindex_doc(uid)
-            result += 1
-    return result
-
-
 @component.adapter(IEntity, IObjectRemovedEvent)
 def _on_entity_removed(entity, event):
     username = entity.username
-    delete_entity_data(username)
+    delete_entity_metadata(dataserver_metadata_catalog(), username)
 
 
 @component.adapter(IEntity, IObjectRemovedEvent)
 def clear_replies_to_creator_when_creator_removed(entity, event):
-    """
-    When a creator is removed, all of the things that were direct
-    replies to that creator are now \"orphans\", with a value
-    for ``inReplyTo``. We clear out the index entry for ``repliesToCreator``
-    for this entity in that case.
-
-    The same scenario holds for things that were shared directly
-    to that user.
-    """
-
-    catalog = dataserver_metadata_catalog()
-    if catalog is None:
-        # Not installed yet
-        return
-
-    # These we can simply remove, this creator doesn't exist anymore
-    for ix_name in (IX_REPLIES_TO_CREATOR, IX_TAGGEDTO):
-        index = catalog[ix_name]
-        query = {ix_name: {'any_of': (entity.username,)}}
-        results = catalog.searchResults(**query)
-        for uid in results.uids:
-            index.unindex_doc(uid)
-
-    # These, though, may still be shared, so we need to reindex them
-    index = catalog[IX_SHAREDWITH]
-    results = catalog.searchResults(sharedWith={'all_of': (entity.username,)})
-    intid_util = results.uidutil
-    uids = list(results.uids or ())
-    for uid in uids:
-        obj = intid_util.queryObject(uid)
-        if obj is not None:
-            index.index_doc(uid, obj)
-
-    index = catalog[IX_REVSHAREDWITH]
-    if IKeywordIndex.providedBy(index):
-        index.remove_words((entity.username,))
+    clear_replies_to_creator(dataserver_metadata_catalog(), entity.username)
