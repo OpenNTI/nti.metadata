f083eb282a1d7a2ceb0aa611e4bcd7b15d5d8f4e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-31 20:14:10 +0000

    remove old generation; kill unicode literals; remove ref to nti.dataserver
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.metadata/trunk@113903 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/metadata/generations/evolve6.py
+++ src/nti/metadata/generations/evolve6.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -12,43 +12,21 @@ logger = __import__('logging').getLogger(__name__)
 generation = 6
 
 from zope import component
-from zope import interface
 
 from zope.component.hooks import setHooks
 from zope.component.hooks import site as current_site
 
 from zope.intid.interfaces import IIntIds
 
-from nti.dataserver.interfaces import IDataserver
-from nti.dataserver.interfaces import IOIDResolver
-
 from nti.metadata import queue_event
 
 from nti.metadata.interfaces import IMetadataQueue
-
-
-@interface.implementer(IDataserver)
-class MockDataserver(object):
-
-    root = None
-
-    def get_by_oid(self, oid, ignore_creator=False):
-        resolver = component.queryUtility(IOIDResolver)
-        if resolver is None:
-            logger.warn("Using dataserver without a proper ISiteManager.")
-        else:
-            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
-        return None
 
 
 def do_evolve(context, generation=generation):
     setHooks()
     conn = context.connection
     ds_folder = conn.root()['nti.dataserver']
-
-    mock_ds = MockDataserver()
-    mock_ds.root = ds_folder
-    component.provideUtility(mock_ds, IDataserver)
 
     lsm = ds_folder.getSiteManager()
     intids = lsm.getUtility(IIntIds)
@@ -70,7 +48,6 @@ def do_evolve(context, generation=generation):
             except AttributeError:
                 pass
 
-    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
     logger.info('Metadata evolution %s done', generation)
         
 
--- src/nti/metadata/generations/tests/test_install.py
+++ src/nti/metadata/generations/tests/test_install.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
@@ -13,7 +13,6 @@ from hamcrest import assert_that
 import unittest
 
 from nti.dataserver.tests import mock_dataserver
-from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
 
 from nti.metadata.tests import SharedConfiguringTestLayer
 
@@ -22,7 +21,7 @@ class TestFunctionalInstall(unittest.TestCase):
 
     layer = SharedConfiguringTestLayer
 
-    @WithMockDSTrans
+    @mock_dataserver.WithMockDSTrans
     def test_installed(self):
         conn = mock_dataserver.current_transaction
         root = conn.root()
